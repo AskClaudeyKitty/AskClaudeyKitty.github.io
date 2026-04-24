fd099f0eba9c7c90497b0cdd7989d578156b7542
noname <noname@example.com>
2026-04-24 14:38:03 -0500

    Pinbark: Arkanoid mode keeps ball speed up, no slowing down

--- pinbark/assets/webworkerAll-DSisUnfg.js
+++ pinbark/assets/webworkerAll-DSisUnfg.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,i as b,M as P,K as I,z as B,$ as T,T as _,ai as O,R as A,w as C,_ as z,p as k}from"./index-3Ma6KXtq.js";import{F as E}from"./Filter-5QwZMRJx.js";var U=`in vec2 aPosition;
+import{G as R,b as S,i as b,M as P,K as I,z as B,$ as T,T as _,ai as O,R as A,w as C,_ as z,p as k}from"./index-C_bV9zEy.js";import{F as E}from"./Filter-DjCR6YhG.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
