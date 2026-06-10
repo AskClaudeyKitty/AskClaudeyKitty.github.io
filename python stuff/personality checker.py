7ddd7f28ea55938a4e83342ab2f572249e47585c
noname <noname@example.com>
2026-06-10 17:16:49 -0500

    Remove zalgo.html

---
+++ python stuff/personality checker.py
@@ -0,0 +1,8 @@
+from random import *
+from time import *
+print('Hello.')
+sleep(1)
+print('What is your name?')
+myname = input()
+sleep(0.5)
+print(f'It is good to meet you, {myname}. My name is questioneer.')
